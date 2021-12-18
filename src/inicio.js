ac88fa5c536402909f22926df64d4572d1df2f8d
Mariano Camposeco <mariano1941@outlook.es>
2021-12-18 16:47:22 -0600

    reestructuramos login

--- src/inicio.js
+++ src/inicio.js
@@ -1,7 +1,7 @@
 
 		$(document).ready(function(){
 			$("#circle").hide();
-			$("#box").animate({height:"280px"},"slow");
+			$("#box").animate({height:"220px"},"slow");
 			$("#box").animate({width:"450px"},"slow");
 			$("#circle").fadeIn(1000);
 
