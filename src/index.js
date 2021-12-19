2633813b0a16c28d3e36d9b9c8ba2a0b493662ca
Pedro Gordillo <pedrogordillo2510@gmail.com>
2021-12-18 19:13:44 -0600

    Cambios Pedro

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path'); 
+
+// server
+const app = express();
+
+// settings
+app.set('port',3000);
+
+//routers
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/index1.ejs'));
+
+});
+
+//middlewares
+
+// statics files
+
+
+//listening the server
+app.listen(app.get('port'), ()=> {
+    console.log("Server in port", app.get('port'));
+    
+} );
