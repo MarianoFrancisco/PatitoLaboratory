33eedd9dad101daaeaea33f18a96122e070c05f3
Mariano Camposeco <mariano1941@outlook.es>
2021-12-19 14:24:09 -0600

    Agregamos pantallas visualizadoras de administrador

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,11 +16,12 @@ router.get('/', (req, res) => {
 router.get('/pruebaCrud', (req, res) => {
   res.render('pruebaCrud', { title: 'Iniciar Sesion' });
 });
+
 router.post('/Proceder', function (req, res) {
 	var nombre = req.body.nombre|| '';	
   var password = req.body.password|| '';	
   if(nombre==administrador&&password==passAdministrador){
-    res.render('administrador', { title: 'Administrador' });
+    res.render('./administrador/administrador', { title: 'Administrador' });
   }else if(nombre==secretaria&&password==passSecretaria){
     res.render('secretaria', { title: 'Secretaria' });
   }else if(nombre==laboratorista&&password==passLaboratorista){
@@ -28,5 +29,24 @@ router.post('/Proceder', function (req, res) {
   }else{
     res.render('index', { title: 'Iniciar Sesion' });
   }
+});
+//router administrador
+router.get('/administrador', (req, res) => {
+  res.render('./administrador/administrador', { title: 'Iniciar Sesion Administrador' });
+});
+router.get('/administrador/examenes', (req, res) => {
+  res.render('./administrador/examenes', { title: 'Examenes Admin' });
+});
+router.get('/administrador/reportes', (req, res) => {
+  res.render('./administrador/reportes', { title: 'Reportes Admin' });
+});
+router.get('/administrador/usuarios', (req, res) => {
+  res.render('./administrador/usuarios', { title: 'Usuarios Admin' });
+});
+router.get('/administrador/corteMes', (req, res) => {
+  res.render('./administrador/corteMes', { title: 'Corte del Mes' });
+});
+router.get('/administrador/roles', (req, res) => {
+  res.render('./administrador/roles', { title: 'Corte del Mes' });
 });
 module.exports = router;
