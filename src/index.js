4b9c01cd275c963311e2437213914f769381f282
Pedro Gordillo <pedrogordillo2510@gmail.com>
2021-12-19 01:01:16 -0600

    Mariano, Douglas y Pedro agregan End point

--- src/index.js
+++ src/index.js
@@ -4,15 +4,9 @@ const path = require('path');
 const router = require('./routes');
 var body_parser = require('body-parser');
 const app = express();
-<<<<<<< HEAD
-
-// settings
-app.set('port', process.env.PORT || 3000);
-=======
 app.use(body_parser.urlencoded({extended:true}));
 // Configuraciòn
 app.set('port', process.env.PORT || 5000);
->>>>>>> Mariano
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -27,9 +21,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 router.post
 // llamado a servidor
 app.listen(app.get('port'), () => {
-<<<<<<< HEAD
-  console.log('Server on port', app.get('port'));
-=======
   console.log('Servidor en puerto: ', app.get('port'));
->>>>>>> Mariano
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const app = express();
-<<<<<<< HEAD
-=======
 //const password = 	document.getElementById("datoPassword");
 
 let administrador = 'Marco';
@@ -11,28 +9,11 @@ let secretaria = 'Juana';
 let passSecretaria='1234';
 let laboratorista = 'Alejandro';
 let passLaboratorista='1234';
->>>>>>> Mariano
 
 router.get('/', (req, res) => {
   res.render('index', { title: 'Iniciar Sesion' });
 });
 
-<<<<<<< HEAD
-router.get('/admin', (req, res) => {
-  res.render('admin', { title: 'Administrador' });
-});
-
-router.get('/saludo', function (req, res) {
-
-	var nombre = req.query.nombre || '';	
-  console.log(nombre);
-  console.log("desde router");
-  res.render('admin', { title: 'Administrador' });
-  
-
-});
-
-=======
 router.post('/Proceder', function (req, res) {
 	var nombre = req.body.nombre|| '';	
   var password = req.body.password|| '';	
@@ -43,9 +24,7 @@ router.post('/Proceder', function (req, res) {
   }else if(nombre==laboratorista&&password==passLaboratorista){
     res.render('laboratorista', { title: 'Laboratorista' });
   }else{
-    
     res.render('index', { title: 'Iniciar Sesion' });
   }
 });
->>>>>>> Mariano
 module.exports = router;
