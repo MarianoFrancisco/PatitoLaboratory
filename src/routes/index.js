671417f89293ca404f7f0d18ff727f72c96379c3
DOU2021 <doueduar2018@gmail.com>
2021-12-21 16:40:48 -0600

    cambio de pestana dinamica

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,7 @@ let passSecretaria='1234';
 let laboratorista = 'Ale';
 let passLaboratorista='1234';
 
+// Iniciar sesion en los diferentes usuarios
 router.get('/', (req, res) => {
   res.render('index', { title: 'Iniciar Sesion' });
 });
@@ -32,9 +33,11 @@ router.get('/horariosSecretaria', (req, res) => {
   res.render('./secretaria/horariosSecretaria', { title: 'Horarios' });
 });
 
+//validar credenciales de los usuarios al momento de logiarse
 router.post('/Proceder', function (req, res) {
 	nombre = req.body.nombre|| '';	
-  var password = req.body.password|| '';	
+  const password = req.body.password|| '';	
+
   if(nombre==administrador&&password==passAdministrador){
     res.render('./administrador/administrador', { title: 'Administrador' });
     console.log("Precondiciones");
@@ -42,16 +45,21 @@ router.post('/Proceder', function (req, res) {
   }else if(nombre==secretaria&&password==passSecretaria){
     res.render('./secretaria/secretariaIndex', { title: 'Secretaria' });
   }else if(nombre==laboratorista&&password==passLaboratorista){
-    res.render('laboratorista', { title: nombre, opcion: 1});
+    res.redirect('/laboratorista');
   }else{
     res.render('index', { title: 'Iniciar Sesion' });
   }
 });
 router.get('/Proceder', function(req,res){
-  const op = req.query.opcion || '';
+});
+
+//rutas de laboratorista
+router.get('/laboratorista', function(req,res){
+  const op = req.query.opcion || 1;
 
   res.render('laboratorista', { title: nombre, opcion: op});
 });
+
 //router administrador
 router.get('/administrador', (req, res) => {
   res.render('./administrador/administrador', { title: 'Iniciar Sesion Administrador' });
