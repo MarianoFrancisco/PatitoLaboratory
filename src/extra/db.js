fe99957d305a7c6c3e495e55dbb5d9e3c895d301
DOU2021 <doueduar2018@gmail.com>
2021-12-28 14:31:47 -0600

    actualizar rama

--- src/extra/db.js
+++ src/extra/db.js
@@ -1,6 +1,6 @@
 const mysqlhost = process.env.MYSQLHOST || 'localhost';
 const mysqluser = process.env.MYSQLUSER || "patitoOliver";
-const mysqlpass = process.env.MYSQLPASS || "Oliver12345";
+const mysqlpass = process.env.MYSQLPASS || "Oliver12345.";
 const mysqldatabase = process.env.MYSQLDATABASE || "patitobd";
 const mysql = require('mysql');
 
