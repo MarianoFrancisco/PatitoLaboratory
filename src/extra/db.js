6a470778f29c05d238e60a87b5fc5febc8bc7df1
Mariano Camposeco <mariano1941@outlook.es>
2021-12-28 04:20:16 -0600

    modificamos visualmente

--- src/extra/db.js
+++ src/extra/db.js
@@ -1,7 +1,7 @@
 const mysqlhost = process.env.MYSQLHOST || 'localhost';
 const mysqluser = process.env.MYSQLUSER || "patitoOliver";
-const mysqlpass = process.env.MYSQLPASS || "Oliver12345.";
-const mysqldatabase = process.env.MYSQLDATABASE || "patitoBD";
+const mysqlpass = process.env.MYSQLPASS || "Oliver12345";
+const mysqldatabase = process.env.MYSQLDATABASE || "patitobd";
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
